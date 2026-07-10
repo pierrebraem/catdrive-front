3b04d350cc67d789d3e1b0e1c18b322afda4c53d
Pierre Braem <pierrebraem@orange.fr>
2026-07-10 16:59:36 +0200

    Feat: pre-commit

---
+++ eslint.config.mjs
@@ -0,0 +1,66 @@
+import { defineConfig } from 'eslint/config'
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+
+export default defineConfig(
+    {
+        files: ['**/*.{js,ts,vue}'],
+        extends: [js.configs.recommended, tseslint.configs.recommended]
+    },
+    {
+        rules: {
+            "no-duplicate-imports": "error",
+            "no-self-compare": "error",
+            "no-console": "error",
+            "no-else-return": "error",
+            "no-empty-function": "error",
+            "no-unassigned-vars": "error",
+            "no-var": "error",
+            "no-script-url": "error",
+            "no-eval": "error",
+            "no-alert": "error",
+            "func-names": "error",
+            "init-declarations": "error",
+            "no-implied-eval": "error",
+            "no-loop-func": "error",
+            "no-multi-assign": "error",
+            "prefer-const": "error",
+            "semi": ["error", "never"]
+        }
+    },
+
+    pluginVue.configs["flat/recommended"],
+    {
+        rules: {
+            "vue/multi-word-component-names": "off",
+            "vue/block-order": [
+                "error",
+                {
+                    "order": ["script", "template", "style"]
+                }
+            ],
+            "vue/no-template-target-blank": "error",
+            "vue/no-undef-components": "error",
+            "vue/no-undef-properties": "error",
+            "vue/no-unsupported-features": [
+                "error",
+                {
+                    version: "^3.4.0"
+                }
+            ],
+            "vue/no-unused-emit-declarations": "error",
+            "vue/no-unused-properties": "error",
+            "vue/no-unused-refs": "error",
+            "vue/no-useless-mustaches": "error",
+            "vue/no-useless-v-bind": "error",
+            "vue/no-v-text": "error",
+            "vue/no-parsing-error": [
+                "error", 
+                {
+                    "invalid-first-character-of-tag-name": false
+                }
+            ]   
+        }
+    }
+)
