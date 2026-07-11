0c981b95fdacca08a236fbde656c846728249097
Pierre Braem <pierrebraem@orange.fr>
2026-07-11 13:55:43 +0200

    Test: detect errors in SonarQube Scan

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,17 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const test = NaN
+</script>
 
 <template>
   <h1>You did it!</h1>
   <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    Visit <a
+      href="https://vuejs.org/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >vuejs.org</a> to read the
     documentation
+    {{ test }}
   </p>
 </template>
 
