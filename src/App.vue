0273d4b238383f16368f5067e2de544cf95b7e25
Pierre Braem <pierrebraem@orange.fr>
2026-07-11 14:05:32 +0200

    Test: Delete bad code

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,4 @@
-<script setup lang="ts">
-const test = NaN
-
-function calculateA(price) {
-  const tax = price * 0.2
-  return price + tax
-}
-
-function calculateB(price) {
-  const tax = price * 0.2
-  return price + tax
-}
-
-function calculateC(price) {
-  const tax = price * 0.2
-  return price + tax
-}
-
-function calculateD(price) {
-  const tax = price * 0.2
-  return price + tax
-}
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <h1>You did it!</h1>
@@ -31,12 +9,5 @@ function calculateD(price) {
       rel="noopener noreferrer"
     >vuejs.org</a> to read the
     documentation
-    {{ test }}
-    {{ calculateA(1) }}
-    {{ calculateB(2) }}
-    {{ calculateC(3) }}
-    {{ calculateD(4) }}
   </p>
 </template>
-
-<style scoped></style>
