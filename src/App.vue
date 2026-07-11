3ba897f4f4765f1177487aeffc9f92830fdb025c
Pierre Braem <64416044+pierrebraem@users.noreply.github.com>
2026-07-11 20:15:08 +0200

    Implement Vitest (#3)
    * Feat: Configurate Vitest
    
    * Feat: Implement tests in GitHub Actions
    
    * Fix: Install playwright
    
    * Refacto: Synchronize jobs 'sonarqube' and 'testing'
    
    * Feat: Run test on multiple browsers
    
    * Fix: Install system dependencies for Actions
    
    * Fix: SonarQube coverage code detection
    
    * Test: if tests failed
    
    * Refacto: Restore good tests

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue"
+
+const count = ref(0)
+
+function increase() {
+  return count.value ++
+}
+
+function decrease() {
+  return count.value --
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a
-      href="https://vuejs.org/"
-      target="_blank"
-      rel="noopener noreferrer"
-    >vuejs.org</a> to read the
-    documentation
-  </p>
+  <button @click="increase">
+    +
+  </button>
+  {{ count }}
+  <button @click="decrease">
+    -
+  </button>
 </template>
---
+++ vitest.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vitest/config'
+import { playwright } from '@vitest/browser-playwright'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+    plugins: [vue()],
+    test: {
+        browser: {
+            provider: playwright(),
+            enabled: true,
+            headless: true,
+            instances: [
+                { browser: 'chromium' },
+                { browser: 'firefox' },
+                { browser: 'webkit' },
+            ]
+        },
+        coverage: {
+            provider: 'istanbul',
+            reporter: ['lcov', 'text', 'html'],
+            reportsDirectory: 'coverage'
+        }
+    }
+})
